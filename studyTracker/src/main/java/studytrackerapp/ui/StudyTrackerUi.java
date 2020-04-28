35bfa4a59d9e70477a265783db1c2015b2cd1f02
Joel Hassan <joel.nhn@gmail.com>
2020-04-28 17:01:33 +0300

    cmon

--- studyTracker/src/main/java/studytrackerapp/ui/StudyTrackerUi.java
+++ studyTracker/src/main/java/studytrackerapp/ui/StudyTrackerUi.java
@@ -161,7 +161,7 @@ public class StudyTrackerUi extends Application {
      */
 
     window.setTitle("Study Tracker");
-    loginScene.getStylesheets().add(getClass().getResource("css/login.css").toExternalForm());
+    // loginScene.getStylesheets().add(getClass().getResource("css/login.css").toExternalForm());
     window.setScene(loginScene);
     window.show();
     // prevents closing of app prior to logout
